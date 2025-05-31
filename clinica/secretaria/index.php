523c01dbbf75333dbe1154c5f8ed02d7af3d12cd
Manuela <manuelamouta1805@gmail.com>
2025-05-31 13:52:11 -0300

    Correção

--- clinica/secretaria/index.php
+++ clinica/secretaria/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Painel da Secretária - Conclínica</title>
+    <title>Painel da Secretária</title>
     <style>
         :root {
             --primary-dark: #693E7F;
