c843d101bbb394b4af87a1d484c65e44bd739752
roberta-blaese <blaeseroberta@gmail.com>
2025-11-22 14:39:42 -0300

    ajustes finais secretaria

--- clinica/secretaria/Consulta/lista_consulta.php
+++ clinica/secretaria/Consulta/lista_consulta.php
@@ -1,40 +1,36 @@
-<?php 
+<?php
 require_once("../Classes/Consulta.class.php");
-$busca = isset($_GET['busca']) ? $_GET['busca'] : "";
-$tipo = isset($_GET['tipo']) ? $_GET['tipo'] : 0;
+
+if (isset($_GET['excluir'])) {
+    $id = intval($_GET['excluir']);
+    $c = new Consulta($id, '', '', '', '');
+    $c->excluir();
+    header("Location: lista_consulta.php");
+    exit;
+}
+
+$busca = $_GET['busca'] ?? "";
+$tipo = $_GET['tipo'] ?? 0;
 
 $lista = Consulta::listar($tipo, $busca);
+
 $itens = '';
-foreach($lista as $consulta) {
+
+foreach ($lista as $consulta) {
     $item = file_get_contents('itens_listagem_consultas.html');
-    
+
     $item = str_replace('{idconsulta}', $consulta->getConsulta(), $item);
-    $item = str_replace('{status}', formatarStatus($consulta->getStatus()), $item);
-    $item = str_replace('{data_hora}', formatarDataHora($consulta->getDataHora()), $item);
+    $item = str_replace('{status}', $consulta->getStatus(), $item);
     $item = str_replace('{idpaciente}', $consulta->getPaciente(), $item);
     $item = str_replace('{idmedico}', $consulta->getMedico(), $item);
-    
+    $item = str_replace('{data_hora}', $consulta->getDataHora(), $item);
+
     $itens .= $item;
 }
 
-$listagem = file_get_contents('listagem_consulta.html');
-$listagem = str_replace('{itens}', $itens, $listagem);
-print($listagem);
+$html = file_get_contents('listagem_consulta.html');
+$html = str_replace('{itens}', $itens, $html);
 
-// Helper function to format date
-function formatarDataHora($data_hora) {
-    if (empty($data_hora)) return "";
-    $date = new DateTime($data_hora);
-    return $date->format('d/m/Y H:i');
-} 
+echo $html;
 
-function formatarStatus($status) {
-    switch ($status) {
-        case '1': return 'Confirmada';
-        case '2': return 'Não confirmada';
-        default:  return 'Desconhecido';
-    }
-}
-
-?>
 
